3b24d15a16eec0ba932058442ef787f0fef341d7
vishwas <vishwas.muninarayana@emc.com>
2018-11-12 13:43:55 +0530

    logs config updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO `devops_statics_jenkins`.`job_meta_data` (`job_name`,`deploy_stage_name`) VALUES ('mdthelloworld','Deploy to Prod');
+INSERT INTO `devops_statics_jenkins`.`job_meta_data` (`job_name`,`deploy_stage_name`) VALUES ('project_cycle','Deploy');
